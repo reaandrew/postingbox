f8de35b13637b3c441c7bc5e0611ce4823770516
Andrew Rea <andrew.rea@hp.com>
2013-09-27 08:01:42 +0100

    Did not commit everything

--- app.js
+++ app.js
@@ -43,13 +43,14 @@ app.post('/', function(req,res){
     res.send(200);
 });
 
+/*
 io.sockets.on('connection', function (socket) {
-    
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
      console.log(data);
     });
 });
+*/
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
